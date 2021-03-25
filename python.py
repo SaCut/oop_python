4a6504f5fce9d20256c726fff15ee28fbb2d638b
Saverio <saveriocutrupi@users.noreply.github.com>
2021-03-25 12:36:22 +0000

    updated python.py file

--- python.py
+++ python.py
@@ -6,11 +6,18 @@ class Python(Snake):
 		self.large = True
 		self.two_lungs = True
 
+		self.venom = False # polymorphism for attributes. The previous value of the attribute has been changed
+
+
 	def climb(self):
 		return "up we go!"
 
 	def swallow(self):
 		return "can't be bothered to chew"
+
+	# polymorphism for methods. The previous value of the method is changed
+	def hunt():
+		return "very strong hugs"
 
 if __name__=='__main__':
 	python_object = Python()
